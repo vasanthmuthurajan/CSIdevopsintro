1b63fb7d5a1f62538638599be66e4505a089ee98
vasanthmuthurajan <vasanthmuthurajan8021@gmail.com>
2024-05-06 23:29:16 +0530

    addit files and initial commit

---
+++ ABCorg2/f1.py
@@ -0,0 +1,1 @@
+print("Hello world! This is devops assignment file 1.") 
---
+++ ABCorg2/f2.py
@@ -0,0 +1,2 @@
+print("Hello world! This is devops assignment file 2.") 
+Print("Hello world! I'm adding a new line in file 2.") 
